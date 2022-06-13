63f1386549e0a34dad546724038e93b78c28bb68
AbhinavJaintle <2020kucp1018@iiitkota.ac.in>
2022-06-13 23:26:07 +0530

    changes... done

--- sip/urls.py
+++ sip/urls.py
@@ -1,11 +1,11 @@
 from . import views
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url
+
 
 
 urlpatterns = [
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('', include("Home.urls")),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
